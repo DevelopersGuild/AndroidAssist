231c13700d338c1ced0270e4c9639ea4bf7ee5f7
SpencerDo <magicdo7@gmail.com>
2015-10-27 20:50:07 -0700

    adds extra libraries for later use, and alters gitignore file to drop auto-generated files created by IntelliJ.

--- app/src/main/java/com/assist/dg/androidassist/Fragment/CollegeUnivFragment.java
+++ app/src/main/java/com/assist/dg/androidassist/Fragment/CollegeUnivFragment.java
@@ -6,17 +6,16 @@ package com.assist.dg.androidassist.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-
 import com.assist.dg.androidassist.Activity.MainActivity;
-import com.assist.dg.androidassist.R;
 import com.assist.dg.androidassist.Parser.UnivParser;
-
+import com.assist.dg.androidassist.R;
 import java.util.ArrayList;
 
 /**
@@ -24,91 +23,84 @@ import java.util.ArrayList;
  */
 public class CollegeUnivFragment extends Fragment {
 
-    ArrayList<String> listOfColleges;
-    ArrayList<String> listOfUniversities;
-    ArrayList<String> listOfUnivValues;
-    Spinner fromCollegeSpinner;
-    Spinner toUniversitySpinner;
+  ArrayList<String> listOfColleges;
+  ArrayList<String> listOfUniversities;
+  ArrayList<String> listOfUnivValues;
+  Spinner fromCollegeSpinner;
+  Spinner toUniversitySpinner;
 
-    UnivParser univParser;
-    public String selectedUnivValue;
+  UnivParser univParser;
+  public String selectedUnivValue;
 
-    public CollegeUnivFragment() {
+  public CollegeUnivFragment() {
 
-        //Populate the colleges and univs list with dummy data
-        String dummy;
-        listOfColleges = new ArrayList<String>();
-        listOfUniversities = new ArrayList<String>();
-        listOfUnivValues = new ArrayList<String>();
+    //Populate the colleges and univs list with dummy data
+    String dummy;
+    listOfColleges = new ArrayList<String>();
+    listOfUniversities = new ArrayList<String>();
+    listOfUnivValues = new ArrayList<String>();
 
-        listOfColleges.add("Choose a Community College");
-        for (int i=0; i<3; i++){
-            dummy = "Community College " + i;
-            listOfColleges.add(dummy);
-        }
-
-        listOfUniversities.add("Choose a University");
-        for (int i=0; i<3; i++){
-            dummy = "University" + i;
-            listOfUniversities.add(dummy);
-        }
-
-        //Use real data, if it works, delete above code
-        univParser = new UnivParser();
-        univParser.execute(listOfUniversities, listOfUnivValues);
-
-
+    listOfColleges.add("Choose a Community College");
+    for (int i = 0; i < 3; i++) {
+      dummy = "Community College " + i;
+      listOfColleges.add(dummy);
     }
 
-    class SpinnerItemSelectedListener implements AdapterView.OnItemSelectedListener {
-
-        @Override
-        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-            Spinner spinner = (Spinner) parent;
-
-            if(spinner.getId() == R.id.from_college_spinner)
-            {
-                if (position != 0){
-                    toUniversitySpinner.setVisibility(View.VISIBLE);
-                }
-            }
-            else if(spinner.getId() == R.id.to_university_spinner)
-            {
-                if (position != 0) {
-                    selectedUnivValue = listOfUnivValues.get(position);
-                    System.out.println(selectedUnivValue);
-                    ((MainActivity) getActivity()).setSelectedUnivValue(selectedUnivValue);
-                    ((MainActivity) getActivity()).showMajorFragment();
-                }
-            }
-        }
-
-        @Override
-        public void onNothingSelected(AdapterView<?> parent) {
-
-        }
+    listOfUniversities.add("Choose a University");
+    for (int i = 0; i < 3; i++) {
+      dummy = "University" + i;
+      listOfUniversities.add(dummy);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        SpinnerItemSelectedListener listener = new SpinnerItemSelectedListener();
+    //Use real data, if it works, delete above code
+    univParser = new UnivParser();
+    univParser.execute(listOfUniversities, listOfUnivValues);
+  }
 
-        fromCollegeSpinner = (Spinner) rootView.findViewById(R.id.from_college_spinner);
-        ArrayAdapter<String> collegeAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, listOfColleges);
-        fromCollegeSpinner.setAdapter(collegeAdapter);
+  class SpinnerItemSelectedListener implements AdapterView.OnItemSelectedListener {
 
-        toUniversitySpinner = (Spinner) rootView.findViewById(R.id.to_university_spinner);
-        ArrayAdapter<String> universityAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, listOfUniversities);
-        toUniversitySpinner.setAdapter(universityAdapter);
+    @Override public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+      Spinner spinner = (Spinner) parent;
 
-
-        fromCollegeSpinner.setOnItemSelectedListener(listener);
-        toUniversitySpinner.setOnItemSelectedListener(listener);
-
-        return rootView;
+      if (spinner.getId() == R.id.from_college_spinner) {
+        if (position != 0) {
+          toUniversitySpinner.setVisibility(View.VISIBLE);
+        }
+      } else if (spinner.getId() == R.id.to_university_spinner) {
+        if (position != 0) {
+          selectedUnivValue = listOfUnivValues.get(position);
+          System.out.println(selectedUnivValue);
+          ((MainActivity) getActivity()).setSelectedUnivValue(selectedUnivValue);
+          ((MainActivity) getActivity()).showMajorFragment();
+        }
+      }
     }
 
+    @Override public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+  }
+
+  @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+    SpinnerItemSelectedListener listener = new SpinnerItemSelectedListener();
+
+    fromCollegeSpinner = (Spinner) rootView.findViewById(R.id.from_college_spinner);
+    ArrayAdapter<String> collegeAdapter =
+        new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,
+            listOfColleges);
+    fromCollegeSpinner.setAdapter(collegeAdapter);
+
+    toUniversitySpinner = (Spinner) rootView.findViewById(R.id.to_university_spinner);
+    ArrayAdapter<String> universityAdapter =
+        new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,
+            listOfUniversities);
+    toUniversitySpinner.setAdapter(universityAdapter);
+
+    fromCollegeSpinner.setOnItemSelectedListener(listener);
+    toUniversitySpinner.setOnItemSelectedListener(listener);
+
+    return rootView;
+  }
 }
