0ebbb02dc3ffae47d07effd165b1a7db351b71aa
SpencerDo <magicdo7@gmail.com>
2015-11-20 11:15:17 -0800

    moves drawer view and navigation view to BaseActivity

--- app/src/main/java/com/assist/dg/androidassist/Activity/BaseActivity.java
+++ app/src/main/java/com/assist/dg/androidassist/Activity/BaseActivity.java
@@ -1,7 +1,14 @@
 package com.assist.dg.androidassist.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.assist.dg.androidassist.R;
 import com.assist.dg.androidassist.fragment.BaseFragment;
 
@@ -10,9 +17,16 @@ import com.assist.dg.androidassist.fragment.BaseFragment;
  */
 public abstract class BaseActivity extends AppCompatActivity {
 
+  @Bind(R.id.base_navigation) NavigationView navigationView;
+  @Bind(R.id.base_drawer) DrawerLayout drawerLayout;
+  @Bind(R.id.toolbar) Toolbar toolbar;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_base);
+    ButterKnife.bind(this);
+    setSupportActionBar(toolbar);
+
     try {
       getFragmentManager().beginTransaction()
           .add(R.id.activity_layout, createBaseFragment())
@@ -20,7 +34,25 @@ public abstract class BaseActivity extends AppCompatActivity {
     } catch (Exception e) {
       e.printStackTrace();
     }
+
+    final ActionBar ab = getSupportActionBar();
+    if (ab != null) {
+      ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+      ab.setDisplayHomeAsUpEnabled(true);
+    }
+    setupDrawerContent(navigationView);
   }
 
   public abstract BaseFragment createBaseFragment();
+
+  private void setupDrawerContent(final NavigationView navigationView) {
+    navigationView.setNavigationItemSelectedListener(
+        new NavigationView.OnNavigationItemSelectedListener() {
+          @Override public boolean onNavigationItemSelected(MenuItem menuItem) {
+            menuItem.setChecked(true);
+            drawerLayout.closeDrawers();
+            return true;
+          }
+        });
+  }
 }
--- app/src/main/java/com/assist/dg/androidassist/Fragment/CollegeListFragment.java
+++ app/src/main/java/com/assist/dg/androidassist/Fragment/CollegeListFragment.java
@@ -3,13 +3,8 @@ package com.assist.dg.androidassist.fragment;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
 import butterknife.Bind;
 import com.assist.dg.androidassist.R;
@@ -28,11 +23,7 @@ import retrofit.Retrofit;
 public class CollegeListFragment extends BaseFragment
     implements CollegeListAdapter.OnCollegeItemClickListener {
 
-  @Bind(R.id.toolbar) Toolbar toolbar;
   @Bind(R.id.college_list) RecyclerView list;
-  @Bind(R.id.college_navigation) NavigationView navigationView;
-  @Bind(R.id.college_drawer) DrawerLayout drawerLayout;
-
   private CollegeListAdapter adapter;
 
   @Override protected int getLayoutId() {
@@ -41,15 +32,6 @@ public class CollegeListFragment extends BaseFragment
 
   @Override public void onViewCreated(View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    getBaseActivity().setSupportActionBar(toolbar);
-
-    final ActionBar ab = getBaseActivity().getSupportActionBar();
-    if (ab != null) {
-      ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-      ab.setDisplayHomeAsUpEnabled(true);
-    }
-
-    setupDrawerContent(navigationView);
 
     adapter = new CollegeListAdapter();
     LinearLayoutManager m =
@@ -79,16 +61,5 @@ public class CollegeListFragment extends BaseFragment
 
   @Override public void onClickCollegeItem(College c) {
     WLog.i("selected college: " + c.getName());
-  }
-
-  private void setupDrawerContent(final NavigationView navigationView) {
-    navigationView.setNavigationItemSelectedListener(
-        new NavigationView.OnNavigationItemSelectedListener() {
-          @Override public boolean onNavigationItemSelected(MenuItem menuItem) {
-            menuItem.setChecked(true);
-            drawerLayout.closeDrawers();
-            return true;
-          }
-        });
   }
 }
